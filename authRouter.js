29377eeaf7ebfaafc565318259930bc4450385ca
Alexander <sklevzhic@gmail.com>
2021-11-21 17:02:19 +0300

    fix

--- authRouter.js
+++ authRouter.js
@@ -5,7 +5,7 @@ const { check } = require('express-validator')
 
 router.post('/registration', [
     check('username', "Имя пользователя не может быть пустым").notEmpty(),
-    check('password', "Пароль должен быть больше 4 символов и не более 10").isLength({min: 4, max: 10}),
+    check('password', "Пароль1 должен быть больше 4 символов и не более 10").isLength({min: 4, max: 10}),
 ] , controller.registration)
 router.post('/login', controller.login)
 router.get('/users', controller.getUsers)
