5875b4fdd6573a8225274536d8d29395abf96a5d
Alexander <sklevzhic@gmail.com>
2021-11-21 17:36:17 +0300

    fix

--- authRouter.js
+++ authRouter.js
@@ -9,5 +9,6 @@ router.post('/registration', [
 ] , controller.registration)
 router.post('/login', controller.login)
 router.get('/users', controller.getUsers)
+router.get('/', (req,res) =>  res.send("hello"))
 
 module.exports = router
